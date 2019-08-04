af63e3736f5dff940756c1d75af95c91cd2550b7
Kundius Ruslan <kundius.ruslan@gmail.com>
2019-08-04 14:02:15 +0300

    upd

--- index.php
+++ index.php
@@ -7,24 +7,22 @@
         <div class="wrapper">
             <?php get_template_part('partials/header'); ?>
 
+            <?php if (have_posts()) : while ( have_posts() ) : the_post(); ?>
             <div class="container">
                 <div class="breadcrumbs">
                     <?php bcn_display() ?>
                 </div>
+                <h1><?php the_title(); ?></h1>
             </div>
 
             <div class="section-content">
                 <div class="container container_small">
                     <div class="content">
-                        <?php if (have_posts()) : while ( have_posts() ) : the_post(); ?>
-                            <h1><?php the_title(); ?></h1>
-                            <?php the_content(); ?>
-                        <?php endwhile; else: ?>
-                            <p>Извините, ничего не найдено.</p>
-                        <?php endif; ?>
+                        <?php the_content(); ?>
                     </div>
                 </div>
             </div>
+            <?php endwhile; endif; ?>
 
             <?php get_template_part('partials/footer'); ?>
         </div>
