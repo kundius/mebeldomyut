cfb5e23c0eb93e006e022f86094b73bba37e8ab4
Kundius Ruslan <kundius.ruslan@gmail.com>
2019-09-19 17:25:58 +0300

    upd

--- single-product.php
+++ single-product.php
@@ -12,11 +12,43 @@
                 <div class="breadcrumbs">
                     <?php bcn_display() ?>
                 </div>
-                <h1>product <?php the_title(); ?></h1>
+                <h1><?php the_title(); ?></h1>
             </div>
 
             <div class="section-content">
                 <div class="container container_small">
+                    <div class="product-details">
+                        <div class="product-details__image">
+                            <?php the_post_thumbnail('full') ?>
+                        </div>
+                        <div class="product-details__info">
+                            <div class="product-details__title">
+                                <?php the_title() ?>
+                            </div>
+                            <div class="product-details__content">
+                                <?php echo $content ?>
+                            </div>
+                            <?php if ($price = get_field('price')): ?>
+                            <div class="product-details__price">
+                                <?php echo $price['before'] ?>
+                                <span><?php echo number_format($price['value'], 0, ',', ' ') ?></span>
+                                <?php echo $price['after'] ?>
+                            </div>
+                            <?php endif; ?>
+                        </div>
+
+                        <div class="product-details__nav">
+                            <div class="eraser eraser_dark">
+                                <?php if ($next = get_next_post(true, '', 'product_category')): ?>
+                                <div class="eraser__left js-product-details" data-id="<?php echo $next->ID ?>"></div>
+                                <?php endif; ?>
+                                <?php if ($previous = get_previous_post(true, '', 'product_category')): ?>
+                                <div class="eraser__right js-product-details" data-id="<?php echo $previous->ID ?>"></div>
+                                <?php endif; ?>
+                                <div class="eraser__center"></div>
+                            </div>
+                        </div>
+                    </div>
                     <div class="content">
                         <?php the_content(); ?>
                     </div>
