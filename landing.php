30580b946f7d72bf59c883bfd94cb3727938c885
Kundius Ruslan <kundius.ruslan@gmail.com>
2019-09-26 13:00:56 +0300

    upd

--- landing.php
+++ landing.php
@@ -284,7 +284,7 @@ if ($projects) {
                                 <div class="video-item js-video-first"><?php echo $review['media'] ?></div>
                                 <?php endif; ?>
                                 <?php if ($review['type'] == 'image'): ?>
-                                <a href="<?php echo $review['image']['url'] ?>"" class="image-item zoom-container">
+                                <a href="<?php echo $review['image']['sizes']['large'] ?>"" class="image-item zoom-container">
                                     <img src="<?php echo $review['image']['sizes']['medium'] ?>" alt="">
                                 </a>
                                 <?php endif; ?>
