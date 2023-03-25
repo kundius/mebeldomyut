637e896db963edd2e2cdd09b35c180e6e03c1101
kundius <kundius.ruslan@gmail.com>
2023-03-25 23:45:48 +0300

    upd

--- partials/landing/catalogs.php
+++ partials/landing/catalogs.php
@@ -1,7 +1,9 @@
-<div class="container">
-  <div class="catalogs">
-    <div class="catalogs__title">Хотите посмотреть каталоги?</div>
-    <div class="catalogs__desc">Введите Вашу электронную почту, и мы пришлем Вам каталоги с мебелью</div>
-    <?php echo do_shortcode('[contact-form-7 id="8"]'); ?>
+<div class="section-catalogs">
+  <div class="container">
+    <div class="catalogs">
+      <div class="catalogs__title">Хотите посмотреть каталоги?</div>
+      <div class="catalogs__desc">Введите Вашу электронную почту, и мы пришлем Вам каталоги с мебелью</div>
+      <?php echo do_shortcode('[contact-form-7 id="8"]'); ?>
+    </div>
   </div>
 </div>
--- partials/landing/contacts.php
+++ partials/landing/contacts.php
@@ -1,10 +1,12 @@
-<div class="container">
-  <div class="contacts">
-    <div class="contacts__left">
-        <div class="contacts__title">Контакты:</div>
-    </div>
-    <div class="contacts__right">
-        <?php the_page_content(140) ?>
+<div class="section-contacts">
+  <div class="container">
+    <div class="contacts">
+      <div class="contacts__left">
+          <div class="contacts__title">Контакты:</div>
+      </div>
+      <div class="contacts__right">
+          <?php the_page_content(140) ?>
+      </div>
     </div>
   </div>
 </div>
