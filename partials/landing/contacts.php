598f816aaf7ac0f89a3ad0c70dc7b39c4fe43559
kundius <kundius.ruslan@gmail.com>
2023-03-25 21:46:27 +0300

    upd

--- partials/landing/contacts.php
+++ partials/landing/contacts.php
@@ -1,8 +1,10 @@
-<div class="contacts">
+<div class="container">
+  <div class="contacts">
     <div class="contacts__left">
         <div class="contacts__title">Контакты:</div>
     </div>
     <div class="contacts__right">
         <?php the_page_content(140) ?>
     </div>
+  </div>
 </div>
