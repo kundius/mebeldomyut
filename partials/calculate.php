d719e26b91dc2c475648a140cde8886639b20b7f
Kundius Ruslan <kundius.ruslan@gmail.com>
2023-06-02 15:27:39 +0300

    upd

--- partials/calculate.php
+++ partials/calculate.php
@@ -1,5 +1,26 @@
+<?php
+$isWardrobe = in_array(wp_get_post_categories(3, get_the_ID(), ['fields' => 'ids']));
+$isKitchen = in_array(wp_get_post_categories(2, get_the_ID(), ['fields' => 'ids']));
+
+// $isWardrobe = 
+// kitchen
+// $views = [];
+// $materials = [];
+// $title = "Заявка на расчет стоимости";
+// $title = "Заявка на расчет стоимости";
+
+?>
+
 <div id="calculate" class="calculate"> <!-- data-element-class="slbContentEl" -->
-  <div class="calculate__title">Стоимость изготовления шкафа</div>
+  <div class="calculate__title">
+    <?php if ($isWardrobe) : ?>
+      Стоимость изготовления шкафа
+    <?php elseif ($isKitchen) : ?>
+      Стоимость изготовления кухни
+    <?php else : ?>
+      Заявка на расчет
+    <?php endif; ?>
+  </div>
 
   <form action="/wp-json/contact-form-7/v1/contact-forms/1122/feedback" data-reach-goal="otpravka_stoimost" method="post" class="calculate-form js-form">
     <div class="calculate-info">
@@ -12,34 +33,43 @@
     </div>
 
     <div class="calculate-main">
-      <div class="calculate-main__row">
-        <div class="calculate-field">
-          <div class="calculate-field__label">
-            Вид шкафа
-          </div>
-          <div class="calculate-field__control">
-            <select name="view" class="calculate-select">
-              <option value="Шкаф-купе">Шкаф-купе</option>
-              <option value="Шкаф на петлях">Шкаф на петлях</option>
-            </select>
+      <?php if (count($views) > 0) : ?>
+        <div class="calculate-main__row">
+          <div class="calculate-field">
+            <div class="calculate-field__label">
+              Вид шкафа
+            </div>
+            <div class="calculate-field__control">
+              <select name="view" class="calculate-select">
+                <?php foreach ($views as $view) : ?>
+                  <option value="<?php echo $view ?>"><?php echo $view ?></option>
+                <?php endforeach; ?>
+                <!-- <option value="Шкаф на петлях">Шкаф на петлях</option> -->
+              </select>
+            </div>
           </div>
         </div>
-      </div>
+      <?php endif; ?>
 
-      <div class="calculate-main__row">
-        <div class="calculate-field">
-          <div class="calculate-field__label">
-            Материал фасада
-          </div>
-          <div class="calculate-field__control">
-            <select name="material" class="calculate-select">
-              <option value="Зеркальный">Зеркальный</option>
+      <?php if (count($material) > 0) : ?>
+        <div class="calculate-main__row">
+          <div class="calculate-field">
+            <div class="calculate-field__label">
+              Материал фасада
+            </div>
+            <div class="calculate-field__control">
+              <select name="material" class="calculate-select">
+                <?php foreach ($materials as $material) : ?>
+                  <option value="<?php echo $material ?>"><?php echo $material ?></option>
+                <?php endforeach; ?>
+                <!-- <option value="Зеркальный">Зеркальный</option>
               <option value="С пленкой Oracal">С пленкой Oracal</option>
-              <option value="МДФ в пленке ПВХ">МДФ в пленке ПВХ</option>
-            </select>
+              <option value="МДФ в пленке ПВХ">МДФ в пленке ПВХ</option> -->
+              </select>
+            </div>
           </div>
         </div>
-      </div>
+      <?php endif; ?>
 
       <div class="calculate-main__row">
         <div class="calculate-field">
