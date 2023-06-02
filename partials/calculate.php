c77792c0d9e509fb5bddd48e16d56d6d0560ff7e
Kundius Ruslan <kundius.ruslan@gmail.com>
2023-06-02 15:55:16 +0300

    upd

--- partials/calculate.php
+++ partials/calculate.php
@@ -1,5 +1,4 @@
 <?php
-print_r(wp_get_post_terms(get_the_ID(), 'product_category', ['fields' => 'ids']));
 $isWardrobe = in_array(3, wp_get_post_terms(get_the_ID(), 'product_category', ['fields' => 'ids']));
 $isKitchen = in_array(2, wp_get_post_terms(get_the_ID(), 'product_category', ['fields' => 'ids']));
 
@@ -24,81 +23,112 @@ $isKitchen = in_array(2, wp_get_post_terms(get_the_ID(), 'product_category', ['f
   </div>
 
   <form action="/wp-json/contact-form-7/v1/contact-forms/1122/feedback" data-reach-goal="otpravka_stoimost" method="post" class="calculate-form js-form">
-    <div class="calculate-info">
-      <div class="calculate-info__title">
-        Выберите параметры
-      </div>
-      <div class="calculate-info__desc">
-        стоимость за погонный метр измениться от выбора вида шкафа, материала фасада и выбранных метров!
-      </div>
-    </div>
 
-    <div class="calculate-main">
-      <?php if (count($views) > 0) : ?>
+    <?php if ($isWardrobe || $isKitchen) : ?>
+      <div class="calculate-info">
+        <div class="calculate-info__title">
+          Выберите параметры
+        </div>
+        <div class="calculate-info__desc">
+          <?php if ($isWardrobe) : ?>
+            стоимость за погонный метр измениться от выбора вида шкафа, материала фасада и выбранных метров!
+          <?php endif; ?>
+          <?php if ($isKitchen) : ?>
+            стоимость за погонный метр измениться от выбора материала фасада кухни и выбранных метров!
+          <?php endif; ?>
+        </div>
+      </div>
+
+      <div class="calculate-main">
+        <?php if ($isWardrobe) : ?>
+          <div class="calculate-main__row">
+            <div class="calculate-field">
+              <div class="calculate-field__label">
+                Вид шкафа
+              </div>
+              <div class="calculate-field__control">
+                <select name="view" class="calculate-select">
+                  <option value="Шкаф-купе">Шкаф-купе</option>
+                  <option value="Шкаф на петлях">Шкаф на петлях</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        <?php endif; ?>
+
+        <?php if ($isWardrobe) : ?>
+          <div class="calculate-main__row">
+            <div class="calculate-field">
+              <div class="calculate-field__label">
+                Материал фасада
+              </div>
+              <div class="calculate-field__control">
+                <select name="material" class="calculate-select">
+                  <option value="Зеркальный">Зеркальный</option>
+                  <option value="С пленкой Oracal">С пленкой Oracal</option>
+                  <option value="МДФ в пленке ПВХ">МДФ в пленке ПВХ</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        <?php endif; ?>
+
+        <?php if ($isKitchen) : ?>
+          <div class="calculate-main__row">
+            <div class="calculate-field">
+              <div class="calculate-field__label">
+                Материал фасада
+              </div>
+              <div class="calculate-field__control">
+                <select name="material" class="calculate-select">
+                  <option value="МДФ в пленке ПВХ">МДФ в пленке ПВХ</option>
+                  <option value="В пластике ALVIC">В пластике ALVIC</option>
+                  <option value="Эмаль">Эмаль</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        <?php endif; ?>
+
         <div class="calculate-main__row">
           <div class="calculate-field">
             <div class="calculate-field__label">
-              Вид шкафа
+              Метраж мебели
             </div>
             <div class="calculate-field__control">
-              <select name="view" class="calculate-select">
-                <?php foreach ($views as $view) : ?>
-                  <option value="<?php echo $view ?>"><?php echo $view ?></option>
-                <?php endforeach; ?>
-                <!-- <option value="Шкаф на петлях">Шкаф на петлях</option> -->
+              <select name="footage" class="calculate-select">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
               </select>
             </div>
           </div>
         </div>
-      <?php endif; ?>
+      </div>
 
-      <?php if (count($material) > 0) : ?>
-        <div class="calculate-main__row">
-          <div class="calculate-field">
-            <div class="calculate-field__label">
-              Материал фасада
-            </div>
-            <div class="calculate-field__control">
-              <select name="material" class="calculate-select">
-                <?php foreach ($materials as $material) : ?>
-                  <option value="<?php echo $material ?>"><?php echo $material ?></option>
-                <?php endforeach; ?>
-                <!-- <option value="Зеркальный">Зеркальный</option>
-              <option value="С пленкой Oracal">С пленкой Oracal</option>
-              <option value="МДФ в пленке ПВХ">МДФ в пленке ПВХ</option> -->
-              </select>
-            </div>
-          </div>
+      <div class="calculate-result">
+        <div class="calculate-result__cost">
+          <?php if ($isWardrobe) : ?>
+            Стоимость Вашего шкафа от:
+          <?php endif; ?>
+          <?php if ($isKitchen) : ?>
+            Стоимость Вашей кухни от:
+          <?php endif; ?>
+          <span class="calculate-result__cost__price">36 900</span> руб.
         </div>
-      <?php endif; ?>
-
-      <div class="calculate-main__row">
-        <div class="calculate-field">
-          <div class="calculate-field__label">
-            Метраж мебели
-          </div>
-          <div class="calculate-field__control">
-            <select name="footage" class="calculate-select">
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-            </select>
-          </div>
+        <div class="calculate-result__desc">
+          <?php if ($isWardrobe) : ?>
+            Цена за погонный метр измениться от выбора вида шкафа, материала фасада и выбранных метров!
+          <?php endif; ?>
+          <?php if ($isKitchen) : ?>
+            Цена за погонный метр измениться от выбора материала фасада кухни и выбранных метров!
+          <?php endif; ?>
         </div>
       </div>
-    </div>
-
-    <div class="calculate-result">
-      <div class="calculate-result__cost">
-        Стоимость Вашего шкафа от: <span class="calculate-result__cost__price">36 900</span> руб.
-      </div>
-      <div class="calculate-result__desc">
-        Цена за погонный метр измениться от выбора вида шкафа, материала фасада и выбранных метров!
-      </div>
-    </div>
+    <?php endif; ?>
 
     <div class="calculate-contacts">
       <div class="calculate-contacts__row">
