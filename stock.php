b0be0e92276a92f5a67b30a487f7efa1985939f8
Kundius Ruslan <kundius.ruslan@gmail.com>
2024-02-26 16:40:20 +0000

    1

---
+++ stock.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Template Name: Акции
+*/
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php get_template_part('partials/head'); ?>
+    </head>
+    <body class="page">
+        <div class="wrapper">
+            <?php get_template_part('partials/header'); ?>
+
+            <div class="section-white">
+                <div class="container">
+                    section-white
+                </div>
+            </div>
+
+            <?php get_template_part('partials/footer'); ?>
+        </div>
+    </body>
+</html>
